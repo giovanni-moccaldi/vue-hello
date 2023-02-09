3d9d5e671948f790f252a659cbe5ba1809234aaf
giovanni-moccaldi <giovannimoccaldi@gmail.com>
2023-02-09 15:10:31 +0100

    text

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ console.log("ok", Vue);
 const app = Vue.createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            message: 'Hello Vue!hope you will make my life easier'
         }
     }
 });
